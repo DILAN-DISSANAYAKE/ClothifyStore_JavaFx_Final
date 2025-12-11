445472b97c678471ffabb4e1b0ac98d6a804cdf8
DILAN-DISSANAYAKE <dissanayakedilan2005@gmail.com>
2025-12-12 02:32:11 +0530

    feat(auth): Implement basic sign-in and sign-up forms
    Introduce new FXML files for sign-in and sign-up forms, along with their respective controllers. The sign-in form now has a UI with email, password fields, login button, and a link to sign up. Basic client-side validation for empty fields is included in the SignInFromController.

--- src/main/java/controller/SignInFromController.java
+++ src/main/java/controller/SignInFromController.java
@@ -1,5 +1,59 @@
 package controller;
 
-public class SignInFromController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+public class SignInFromController implements Initializable {
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private Hyperlink hypSignUp;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        btnLogin.setOnAction(event -> performLogin());
+
+        hypSignUp.setOnAction(event -> navigateToSignUp());
+    }
+
+    private void performLogin() {
+        String email = txtEmail.getText();
+        String password = txtPassword.getText();
+
+
+        if (email.isEmpty() || password.isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, "Form Error", "Please enter both email and password.");
+        } else {
+            System.out.println("User tried to login");
+        }
+    }
+
+    private void navigateToSignUp() {
+        System.out.println("User clicked Sign Up..!");
+       }
+
+    private void showAlert(Alert.AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }

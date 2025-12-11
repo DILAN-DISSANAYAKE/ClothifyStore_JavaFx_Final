f9dc612de1606ddd22de16551d270dec0b2c284a
DILAN-DISSANAYAKE <dissanayakedilan2005@gmail.com>
2025-12-12 02:23:27 +0530

    feat(config): Add HibernateUtil for session management
    This commit introduces a new `HibernateUtil` class in the `config` package.
    It provides a singleton `SessionFactory` for managing Hibernate sessions
    and registers all necessary entity classes for the application.

---
+++ src/main/java/config/HibernateUtil.java
@@ -0,0 +1,24 @@
+package config;
+
+import model.entity.*;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static SessionFactory sessionFactory;
+    public static SessionFactory getSessionFactory(){
+        if(sessionFactory == null){
+            Configuration configuration=new Configuration();
+            configuration.addAnnotatedClass(Users.class);
+            configuration.addAnnotatedClass(Customer.class);
+            configuration.addAnnotatedClass(Orders.class);
+            configuration.addAnnotatedClass(OrderDetails.class);
+            configuration.addAnnotatedClass(Supplier.class);
+            configuration.addAnnotatedClass(Product.class);
+            configuration.configure();
+            sessionFactory = configuration.buildSessionFactory();
+
+        }
+        return sessionFactory;
+    }
+}
